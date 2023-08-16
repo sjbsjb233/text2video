7becfa4daede07790369e81fc28b479ea9b84802
sjbsjb233 <2930105253@qq.com>
2023-08-16 11:02:16 +0800

    人物lora固定

--- script/sd.py
+++ script/sd.py
@@ -39,6 +39,26 @@ def hires_fix(path: str, save_path: str):
         return False
     return result.info
 
+def figure(path,text):
+    '''人物lora和prompt固定'''
+    # 检查path是否加上/
+    if path[-1] != '/':
+        path += '/'
+    
+    #获取figure表中的所有数据
+    sql='''SELECT * FROM figure'''
+    data=database.select_data(sql, (), '%stemperature.db' % path)
+
+    prompt=''
+    #循环检索任务,人名在上的优先
+    for i in data:
+        #核对人名
+        if i[1] in text or i[2] in text or i[3] in text:
+            #如果人名在文本中
+            prompt="%s,%s"%(i[4],i[5])
+            break
+    return prompt
+
 def sd_queue(path):
     '''本函数提供给多进程监听sd_queue数据库使用'''
     # 检查path是否加上/
@@ -60,6 +80,7 @@ def sd_queue(path):
         database.change_data(sql1, (data[0],), '%stemperature.db' % path)
         #确认operate操作模式
         if data[2] == 0:
+            # 如果是0，说明进行text2img操作
             # 从gpt_queue表中获取completion(main表中的prompt_id为gpt_queue表中的id)
 
             sql = '''SELECT prompt_id FROM main WHERE id=?'''
@@ -69,11 +90,29 @@ def sd_queue(path):
             completion=database.select_data(
                 sql, (prompt_id,), '%stemperature.db' % path)[0][0]
             # sd操作
-            api_rep=text2img(completion, '%simage/%s.png' % (path, data[0]))
-            # 将sd生成的文件路径写入sd_queue表,并将其state更新为4(完成)
-            sql2 = '''UPDATE sd_queue SET path=?,state=4 WHERE id=?'''
+            api_rep=text2img(completion, '%spic/%s.png' % (path, data[0]))
+            # 将sd生成的文件路径写入sd_queue表,并将其state更新为4(完成),并存入api_rep
+            sql2 = '''UPDATE sd_queue SET path=?,state=4,api_rep=? WHERE id=?'''
             database.change_data(
-                sql2, ('%simage/%s.png' % (path, data[0]), data[0]), '%stemperature.db' % path)
+                sql2, ('%spic/%s.png' % (path, data[0]), api_rep, data[0]), '%stemperature.db' % path)
+        elif data[2] == 1:
+            # 如果是1，说明进行hires_fix操作
+            #获取图片路径
+            sql='''SELECT pic_id FROM main WHERE id=?'''
+            pic_id=database.select_data(
+                sql, (data[1],), '%stemperature.db' % path)[0][0]
+            sql='''SELECT path FROM pic_queue WHERE id=?'''
+            path_1=database.select_data(
+                sql, (pic_id,), '%stemperature.db' % path)[0][0]
+            
+            # sd操作
+            api_rep=hires_fix(path_1, '%spic/%s.png' % (path, data[0]))
+
+            # 将sd生成的文件路径写入sd_queue表,并将其state更新为4(完成),并存入api_rep
+            sql2 = '''UPDATE sd_queue SET path=?,state=4,api_rep=? WHERE id=?'''
+            database.change_data(
+                sql2, ('%spic/%s.png' % (path, data[0]), api_rep, data[0]), '%stemperature.db' % path)
+
 
 if __name__ == '__main__':
     print(text2img('masterpiece,4k,(beautiful face),1girl,white background', r'.\test.png'))
